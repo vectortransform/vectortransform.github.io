f7cf0078662a339dbc070e0c8641db3c4eae8162
Accelerator <wc2476@columbia.edu>
2018-11-27 14:36:06 -0500

    Update index.js

--- colorbot/index.js
+++ colorbot/index.js
@@ -98,7 +98,7 @@ class Classifier {
     const inputBuffer = tf.buffer([1, this.maxLen], 'float32');
     for (let i = 0; i < inputText.length; ++i) {
       const word = inputText[i];
-      inputBuffer.set(this.wordIndex[word], 0, i);
+      inputBuffer.set(this.wordIndex[word], 0, this.maxLen-inputText.length+i);
       console.log(word, this.wordIndex[word], inputBuffer);
     }
     const input = inputBuffer.toTensor();
